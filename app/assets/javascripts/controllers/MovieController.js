30c41685d801a104551b4b0b3b11cd766af3179d
matsv339 <matsv339@student.liu.se>
2014-04-30 01:28:21 +0200

    Small fixes

--- app/assets/javascripts/controllers/MovieController.js
+++ app/assets/javascripts/controllers/MovieController.js
@@ -1,11 +1,20 @@
 var movieController = angular.module('movieController', []);
 
-movieController.controller('MovieController', function($scope, $http) {
-	$http.get('/movies.json').success(function(data) {
-		
-  		$scope.movies = [];
+Object.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
 
-  		for(var i = 0;i < 2; i++) {
+movieController.controller('MovieController', function($scope, $http) {
+
+    $http.get('/movies.json').success(function(data) {
+  		$scope.movies = [];
+        size = Object.size(data);
+
+  		for(var i = 0;i < size; i++) {
   			$http.get('http://www.omdbapi.com/?i=' + data[i].imdbID).success(function(data) {
   				$scope.movies.push(data);
   			});
