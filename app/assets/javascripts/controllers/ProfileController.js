d38e122305ac77fa30d5c610a0253cc22bee8a32
matsv339 <matsv339@student.liu.se>
2014-05-30 19:49:06 +0200

    nytt försök

--- app/assets/javascripts/controllers/ProfileController.js
+++ app/assets/javascripts/controllers/ProfileController.js
@@ -1,5 +1,14 @@
-angular.module('movieModule').controller('ProfileController', function($scope, $rootScope, Profile) {
+angular.module('movieModule').controller('ProfileController', function($scope, $rootScope, Profile, Progresses) {
 	$scope.reasent_movies = Profile.last_watched({user_id: $rootScope.current_user.id}, function(data) {
 		$scope.hasHistory = data.length != 0
 	});
+
+	$scope.remove_reasent = function($index) {
+		
+		imdb_id = $scope.reasent_movies[$index].data.imdb_id;
+
+		Progresses.destroy({imdb_id: imdb_id, user_id: $rootScope.current_user.id}, function(data) {
+			$scope.reasent_movies.splice($index,1);
+		});
+	}
 });
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,7 +3,7 @@ class ProfilesController < ApplicationController
 	respond_to :json
 
 	def last_watched
-		last_movies = Progress.where(user_id: params[:user_id]).limit(5).order("updated_at desc")
+		last_movies = Progress.where(user_id: params[:user_id]).limit(6).order("updated_at desc")
 		movies = Array.new
 		last_movies.each do |last_movie|
 			movie = Movie.find_by(imdb_id: last_movie.imdb_id)
